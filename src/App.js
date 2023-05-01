fd2b48d603c152338c2fbcda0ad202462cf93587
mahdie <jafari.mahdieh@gmail.com>
2023-05-01 09:46:12 +0430

    new item can be added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
+import { useState } from "react";
 import { ItemData } from "./components/Item/ItemData";
 import { ItemList } from "./components/Item/ItemList";
 import { NewItem } from "./components/NewItem/NewItem";
 
 function App() {
-  const itemData = ["do do sth", "else", "ffv"];
+  const data = ["do do sth", "else", "ffv"];
+  const [itemData, setItemData] = useState(data);
 
+  const addNewItemHandler = (newItem) => {
+    setItemData((prev) => {
+      return [...prev, newItem];
+    });
+  };
   return (
     <div className="container">
-      <NewItem />
+      <NewItem addNewItemHandler={addNewItemHandler} />
       <ItemList itemData={itemData} />
     </div>
   );
--- src/components/NewItem/NewItem.js
+++ src/components/NewItem/NewItem.js
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "./NewItem.css";
-export const NewItem = () => {
+export const NewItem = (props) => {
+  const [newItem, setNewItem] = useState("");
+  const addGoalHandler = () => {
+    props.addNewItemHandler(newItem);
+    setNewItem("");
+  };
+  const addDataHandler = (e) => {
+    setNewItem(e.target.value);
+  };
   return (
     <div className="new-item">
       <h2 className="new-item__title">Course Goal</h2>
-      <input className="new-item__data" />
-      <button className="new-item__button">Add Goal</button>
+      <input
+        className="new-item__data"
+        onChange={addDataHandler}
+        value={newItem}
+      />
+      <button className="new-item__button" onClick={addGoalHandler}>
+        Add Goal
+      </button>
     </div>
   );
 };
