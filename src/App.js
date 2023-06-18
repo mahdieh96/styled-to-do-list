99a807de9a50ca378388914d4286b6677b01edde
mahdie <jafari.mahdieh@gmail.com>
2023-06-18 11:12:38 +0330

    some css changes

--- src/App.js
+++ src/App.js
@@ -2,26 +2,39 @@ import { useState } from "react";
 import { ItemData } from "./components/Item/ItemData";
 import { ItemList } from "./components/Item/ItemList";
 import { NewItem } from "./components/NewItem/NewItem";
-
+import "./App.css";
 function App() {
-  const data = ["do do sth", "else", "ffv"];
+  const data = [
+    { text: "do do sth", id: "1" },
+    { text: "else", id: "2" },
+  ];
   const [itemData, setItemData] = useState(data);
 
   const addNewItemHandler = (newItem) => {
     setItemData((prev) => {
-      return [...prev, newItem];
+      return [...prev, { text: newItem, id: Math.random().toString() }];
     });
   };
 
-  const deleteItemHandler = (deletedItemIndex) => {
+  const deleteItemHandler = (deletedItemId) => {
     setItemData((prev) => {
-      return prev.filter((item, index) => index != deletedItemIndex);
+      return prev.filter((item) => item.id !== deletedItemId);
     });
   };
-  return (
-    <div className="container">
-      <NewItem addNewItemHandler={addNewItemHandler} />
+  let content = (
+    <p style={{ textAlign: "center" }}>No goals found. Maybe add one?</p>
+  );
+  if (data.length > 0) {
+    content = (
       <ItemList itemData={itemData} deleteItemHandler={deleteItemHandler} />
+    );
+  }
+  return (
+    <div>
+      <section id="goal-form">
+        <NewItem addNewItemHandler={addNewItemHandler} />
+      </section>
+      <section id="goals">{content}</section>
     </div>
   );
 }
